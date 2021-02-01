c47d46af56dadf7a5e414aea276324b6eccf8733
Tonino Kastelan <tonino.kastelan@seekandhit.com>
2021-02-01 14:10:52 +0000

    Add pre-commit hook for github pages deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,27 @@
-const gulp = require('gulp')
-const imagemin = require('gulp-imagemin')
-const concat = require('gulp-concat')
 const terser = require('gulp-terser')
+const gulp = require('gulp')
+const concat = require('gulp-concat')
 const sourcemaps = require('gulp-sourcemaps')
 const change = require('gulp-change')
 const {src, parallel, dest} = require('gulp')
 
-const jsPath = '*.js';
+const jsPath = 'js/*.js';
+
 
 function performChange(content, done){
     let regex = /^<script src="js\/(?:\S|\s(?!<\/body>))+/gims
-    newContent = content.replace(regex, '<script src="js/all.js" type="text/javascript"></script>')
+    newContent = content.replace(regex, '<script src="./all.js" type="text/javascript"></script>')
     done(null, newContent)
 }
 
+
 function copyHtml(){
-    return src('*.html')
+    return src('socialbakers_api_wdc_dev.html')
         .pipe(change(performChange))
-        .pipe(gulp.dest('dist'))
+        .pipe(concat('socialbakers_api_wdc.html'))
+        .pipe(gulp.dest('.'))
 }
 
-function imgTask(){
-    return src('images/*').pipe(imagemin()).pipe(gulp.dest('dist/images'))
-}
 
 function jsTask(){
     return src(jsPath)
@@ -30,8 +29,8 @@ function jsTask(){
         .pipe(concat('all.js'))
         .pipe(terser())
         .pipe(sourcemaps.write('.'))
-        .pipe(dest('dist/js'))
+        .pipe(dest('.'))
 }
 
 
-exports.default = parallel(copyHtml, imgTask, jsTask)
+exports.default = parallel(copyHtml, jsTask)
