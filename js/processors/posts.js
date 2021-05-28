8651c12a7990b7df8eb873aa15ae790935a6a661
ktonino <tonino.kastelan@seekandhit.com>
2021-05-28 12:32:28 +0200

    add gender_age as fake dimension

--- js/processors/posts.js
+++ js/processors/posts.js
@@ -3,6 +3,9 @@ function preparePostsParams(sbksData, network, params) {
     if (params.fields.includes('country')) {
         params.fields.splice(params.fields.indexOf('country'), 1)
     }
+    if (params.fields.includes('gender_age')) {
+        params.fields.splice(params.fields.indexOf('gender_age'), 1)
+    }
     if (sbksData.posts_networks[network].sort) {
         params.sort = params.sort || []
         for (const field of sbksData.posts_networks[network].sort) {
@@ -58,7 +61,7 @@ function processPost(post) {
                 }
 
                 for (const subField of Object.keys(fieldObj.subfields)) {
-                    let formattedSubField = subField.replace(/\+/g, ' ')
+                    let formattedSubField = subField
                         .replace(/./g, '_')
                         .replace(/-/g, '_')
                     row[`${field}_${formattedSubField}_${parseInt(i, 10) + 1}`] = item[subField]
@@ -66,7 +69,7 @@ function processPost(post) {
             }
         } else if (fieldObj.subfields && value) {
             for (const subField of Object.keys(value)) {
-                let formattedSubField = subField.replace(/\+/g, ' ')
+                let formattedSubField = subField
                     .replace(/./g, '_')
                     .replace(/-/g, '_')
                 row[`${field}_${formattedSubField}`] = value[subField]
@@ -78,8 +81,14 @@ function processPost(post) {
 }
 
 
-function processCountriesResponse(post, data){
-    for ([key, value] of Object.entries(post.insights_video_view_time_by_country)) {
+function processEdgeCasesResponse(post, data){
+    let byCountry = post.insights_video_view_time_by_country
+    let byGenderAge = post.insights_video_view_time_by_gender_age
+    let edgeCase = byCountry ? byCountry : byGenderAge
+    if (!edgeCase){
+        return
+    }
+    for ([key, value] of Object.entries(edgeCase)) {
         if (data.hasOwnProperty(key))
             data[key] += value
         else
@@ -104,7 +113,8 @@ async function getPostsData(sbksData) {
     }
 
     let rows = []
-    let countriesData = {}
+    let edgeCaseData = {}
+    let typeOfEdgeCase = null
     await Promise.all(requests)
     for (const request of requests) {
         let apiResponse = await request
@@ -113,16 +123,20 @@ async function getPostsData(sbksData) {
         }
 
         for (const post of apiResponse.data.posts) {
-            if (post.insights_video_view_time_by_country){
-                processCountriesResponse(post, countriesData)
+            if (post.hasOwnProperty('insights_video_view_time_by_country') || post.hasOwnProperty('insights_video_view_time_by_gender_age')){
+                typeOfEdgeCase = post.insights_video_view_time_by_country ? 'country' : 'gender_age'
+                processEdgeCasesResponse(post, edgeCaseData)
                 continue
             }
             rows.push(processPost(post))
         }
     }
     if (rows.length === 0){
-        for ([key, value] of Object.entries(countriesData)) {
-            rows.push({'country': key, 'insights_video_view_time_by_country': value})
+        for ([key, value] of Object.entries(edgeCaseData)) {
+            data = {}
+            data[typeOfEdgeCase] = key
+            data[`insights_video_view_time_by_${typeOfEdgeCase}`] = value
+            rows.push(data)
         }
     }
     return rows
